c79529df7bb6f35292867a8e1355575c7b2e3ad6
shoebhakeem <shoebhakeem123@gmail.com>
2026-03-04 09:28:01 -0600

    Phase 1 - Seeded provinces

---
+++ SQL/99_Seeds/01_Seed_Provinces.sql
@@ -0,0 +1,20 @@
+USE CorpCore_OLTP;
+GO
+
+INSERT INTO oltp.Provinces (ProvinceID, ProvinceCode, ProvinceName)
+VALUES
+(1,'BC','British Columbia'),
+(2,'AB','Alberta'),
+(3,'SK','Saskatchewan'),
+(4,'MB','Manitoba'),
+(5,'ON','Ontario'),
+(6,'QC','Quebec'),
+(7,'NB','New Brunswick'),
+(8,'NS','Nova Scotia'),
+(9,'PE','Prince Edward Island'),
+(10,'NL','Newfoundland and Labrador'),
+(11,'YT','Yukon'),
+(12,'NT','Northwest Territories'),
+(13,'NU','Nunavut');
+GO
+
